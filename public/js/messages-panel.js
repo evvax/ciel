c267a47bd01cdb6ec89e372119fdd7f63ccc4a14
evvax <starsyko16@icloud.com>
2025-12-22 09:18:34 -0500

    Ciel App

---
+++ public/js/messages-panel.js
@@ -0,0 +1,3 @@
+const panel = document.getElementById('messagesPanel');
+setTimeout(() => panel.classList.add('show'), 1000);
+setTimeout(() => panel.classList.remove('show'), 3000);
---
+++ public/js/mic-status.js
@@ -0,0 +1,11 @@
+let micOn = true;
+const micButton = document.querySelector('.mic-btn');
+micButton.addEventListener('click', () => {
+  micOn = !micOn;
+  micButton.classList.toggle('muted', !micOn);
+  if (micOn) {
+    console.log('Microphone ON');
+  } else {
+    console.log('Microphone OFF');
+  }
+});
